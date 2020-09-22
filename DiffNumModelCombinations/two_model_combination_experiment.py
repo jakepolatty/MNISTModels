Fixed accuracy computation bug for CIFAR dataset

DiffNumModelCombinations/two_model_combination_experiment.py
@@ -76,15 +76,15 @@ def run_combined(simple_model, complex_model, inputs, labels, conf_value):
     simple_indices = simple_indices[simple_indices != np.array(None)] # remove None values
     simple_indices = np.asarray(simple_indices, dtype=np.int64)
 
-    reduced_simple_probs = np.take(simple_probs, simple_indices, axis=0)
-    simple_preds = reduced_simple_probs.argmax(axis=1)
+    simple_preds = np.argmax(simple_probs, axis=1)
 
     # ------------------------------------
     # Reorganize preds
     combined_preds = np.arange(inputs.shape[0])
 
-    np.put(combined_preds, simple_indices, simple_preds)
+    np.put(combined_preds, indices, simple_preds)
     np.put(combined_preds, complex_indices, complex_preds)
+    print(combined_preds, labels)
 
     simplePercent = simple_indices.shape[0] / (simple_indices.shape[0] + complex_indices.shape[0])
     return tf.reduce_mean(tf.cast(tf.equal(combined_preds, labels), tf.float32)).numpy(), time.time() - before, simplePercent
@@ -92,6 +92,7 @@ def run_combined(simple_model, complex_model, inputs, labels, conf_value):
 def main():
     print('Loading data...')
     x_train, y_train, x_test, y_test = helpers.get_cifar10_data()
+    y_test = tf.squeeze(y_test)
 
     #train_and_save_models(x_train, y_train)
 

v3/model_data.py
@@ -27,43 +27,43 @@ def main():
     for i in range(num_runs):
         print(i, '/', num_runs)
         t0 = time.time()
-        l1_model = models.get_trained_l1_all_digit_model(train_images, train_labels, epochs=10)
+        l1_model = models.get_trained_l1_all_digit_model(train_images, train_labels, epochs=1)
         l1_model.save('models/l1_cifar10_model' + str(i))
 
         t1 = time.time()
-        l2_model = models.get_trained_l2_all_digit_model(train_images, train_labels, input_shape, epochs=10)
+        l2_model = models.get_trained_l2_all_digit_model(train_images, train_labels, input_shape, epochs=1)
         l2_model.save('models/l2_cifar10_model' + str(i))
 
         t2 = time.time()
-        l3_model = models.get_trained_l3_all_digit_model(train_images, train_labels, input_shape, epochs=10)
+        l3_model = models.get_trained_l3_all_digit_model(train_images, train_labels, input_shape, epochs=1)
         l3_model.save('models/l3_cifar10_model' + str(i))
 
         t3 = time.time()
-        l4_model = models.get_trained_l4_all_digit_model(train_images, train_labels, input_shape, epochs=10)
-        l4_model.save('models/l4_cifar10_model' + str(i))
-        t4 = time.time()
+        # l4_model = models.get_trained_l4_all_digit_model(train_images, train_labels, input_shape, epochs=1)
+        # l4_model.save('models/l4_cifar10_model' + str(i))
+        # t4 = time.time()
 
         l1_mean_train_time += t1 - t0
         l2_mean_train_time += t2 - t1
         l3_mean_train_time += t3 - t2
-        l4_mean_train_time += t4 - t3
+        #l4_mean_train_time += t4 - t3
 
     l1_mean_train_time /= num_runs
     l2_mean_train_time /= num_runs
     l3_mean_train_time /= num_runs
-    l4_mean_train_time /= num_runs
+    #l4_mean_train_time /= num_runs
 
     # Recording test times and accuracies
 
     l1_mean_test_time = 0
     l2_mean_test_time = 0
     l3_mean_test_time = 0
-    l4_mean_test_time = 0
+    #l4_mean_test_time = 0
 
     l1_mean_accuracy = 0
     l2_mean_accuracy = 0
     l3_mean_accuracy = 0
-    l4_mean_accuracy = 0
+    #l4_mean_accuracy = 0
 
     print('Recording test times and accuracies...')
     for i in range(num_runs):
@@ -71,7 +71,7 @@ def main():
         l1_model = tf.keras.models.load_model('./models/l1_cifar10_model' + str(i))
         l2_model = tf.keras.models.load_model('./models/l2_cifar10_model' + str(i))
         l3_model = tf.keras.models.load_model('./models/l3_cifar10_model' + str(i))
-        l4_model = tf.keras.models.load_model('./models/l4_cifar10_model' + str(i))
+        #l4_model = tf.keras.models.load_model('./models/l4_cifar10_model' + str(i))
 
         t0 = time.time()
         l1_mean_accuracy += l1_model.evaluate(test_images, test_labels, verbose=0)[1]
@@ -80,41 +80,41 @@ def main():
         t2 = time.time()
         l3_mean_accuracy += l3_model.evaluate(test_images, test_labels, verbose=0)[1]
         t3 = time.time()
-        l4_mean_accuracy += l4_model.evaluate(test_images, test_labels, verbose=0)[1]
-        t4 = time.time()
+        # l4_mean_accuracy += l4_model.evaluate(test_images, test_labels, verbose=0)[1]
+        # t4 = time.time()
 
         l1_mean_test_time += t1 - t0
         l2_mean_test_time += t2 - t1
         l3_mean_test_time += t3 - t2
-        l4_mean_test_time += t4 - t3
+        #l4_mean_test_time += t4 - t3
 
     l1_mean_test_time /= num_runs
     l2_mean_test_time /= num_runs
     l3_mean_test_time /= num_runs
-    l4_mean_test_time /= num_runs
+    # l4_mean_test_time /= num_runs
 
     l1_mean_accuracy /= num_runs
     l2_mean_accuracy /= num_runs
     l3_mean_accuracy /= num_runs
-    l4_mean_accuracy /= num_runs
+    # l4_mean_accuracy /= num_runs
 
     # print('Training Times')
     print(l1_mean_train_time)
     print(l2_mean_train_time)
     print(l3_mean_train_time)
-    print(l4_mean_train_time)
+    # print(l4_mean_train_time)
 
     print('Testing times')
     print(l1_mean_test_time)
     print(l2_mean_test_time)
     print(l3_mean_test_time)
-    print(l4_mean_test_time)
+    # print(l4_mean_test_time)
 
     print('Testing Accuracies')
     print(l1_mean_accuracy)
     print(l2_mean_accuracy)
     print(l3_mean_accuracy)
-    print(l4_mean_accuracy)
+    # print(l4_mean_accuracy)
 
 if __name__ == '__main__':
     main()