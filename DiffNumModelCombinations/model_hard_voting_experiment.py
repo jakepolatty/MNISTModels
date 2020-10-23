b4de1e83a2d0dc73381e264fd0d31c4282475342
Jake Polatty <jakepolatty@gmail.com>
2020-10-22 20:48:23 -0400

    Converted hard and soft voting experiments to multi-ensemble format

--- DiffNumModelCombinations/model_hard_voting_experiment.py
+++ DiffNumModelCombinations/model_hard_voting_experiment.py
@@ -14,28 +14,81 @@ def main():
     y_test = tf.squeeze(y_test)
 
     print("Loading models...")
-    l1_model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=models.get_l1_model, epochs=10, verbose=True)
+    l1_model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=models.get_l1_model, epochs=10, verbose=False)
     l1_model._estimator_type = "classifier"
-    l2_model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=models.get_l2_model, epochs=10, verbose=True)
+    l2_model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=models.get_l2_model, epochs=10, verbose=False)
     l2_model._estimator_type = "classifier"
-    l3_model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=models.get_l3_model, epochs=10, verbose=True)
+    l3_model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=models.get_l3_model, epochs=10, verbose=False)
     l3_model._estimator_type = "classifier"
-    l4_model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=models.get_l4_model, epochs=10, verbose=True)
+    l4_model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=models.get_l4_model, epochs=10, verbose=False)
     l4_model._estimator_type = "classifier"
 
-    ensemble = VotingClassifier(estimators=[('l1', l1_model),
+    ensemble12 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l2', l2_model)],
+                                voting='hard', verbose=False)
+    ensemble13 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l3', l3_model)],
+                                voting='hard', verbose=False)
+    ensemble14 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l4', l4_model)],
+                                voting='hard', verbose=False)
+    ensemble23 = VotingClassifier(estimators=[('l2', l2_model),
+                                            ('l3', l3_model)],
+                                voting='hard', verbose=False)
+    ensemble24 = VotingClassifier(estimators=[('l2', l2_model),
+                                            ('l4', l4_model)],
+                                voting='hard', verbose=False)
+    ensemble34 = VotingClassifier(estimators=[('l3', l3_model),
+                                            ('l4', l4_model)],
+                                voting='hard', verbose=False)
+
+    ensemble123 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l2', l2_model),
+                                            ('l3', l3_model)],
+                                voting='hard', verbose=False)
+    ensemble124 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l2', l2_model),
+                                            ('l4', l4_model)],
+                                voting='hard', verbose=False)
+    ensemble134 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l3', l3_model),
+                                            ('l4', l4_model)],
+                                voting='hard', verbose=False)
+    ensemble234 = VotingClassifier(estimators=[('l2', l2_model),
+                                            ('l3', l3_model),
+                                            ('l4', l4_model)],
+                                voting='hard', verbose=False)
+
+
+    ensemble1234 = VotingClassifier(estimators=[('l1', l1_model),
                                             ('l2', l2_model),
                                             ('l3', l3_model),
                                             ('l4', l4_model)],
-                                voting='hard')
+                                voting='hard', verbose=False)
 
     l1_model.fit(x_train, y_train)
     l2_model.fit(x_train, y_train)
     l3_model.fit(x_train, y_train)
     l4_model.fit(x_train, y_train)
-    ensemble.fit(x_train, y_train)
 
-    for clf in (l1_model, l2_model, l3_model, l4_model, ensemble):
+    ensemble12.fit(x_train, y_train)
+    ensemble13.fit(x_train, y_train)
+    ensemble14.fit(x_train, y_train)
+    ensemble23.fit(x_train, y_train)
+    ensemble24.fit(x_train, y_train)
+    ensemble34.fit(x_train, y_train)
+
+    ensemble123.fit(x_train, y_train)
+    ensemble124.fit(x_train, y_train)
+    ensemble134.fit(x_train, y_train)
+    ensemble234.fit(x_train, y_train)
+
+    ensemble1234.fit(x_train, y_train)
+
+    for clf in (l1_model, l2_model, l3_model, l4_model,
+                ensemble12, ensemble13, ensemble14, ensemble23, ensemble24, ensemble34,
+                ensemble123, ensemble124, ensemble134, ensemble234,
+                ensemble1234):
         before_time = time.time()
         y_pred = clf.predict(x_test)
         model_time = time.time() - before_time
--- DiffNumModelCombinations/model_soft_voting_experiment.py
+++ DiffNumModelCombinations/model_soft_voting_experiment.py
@@ -23,11 +23,78 @@ def main():
     l4_model = tf.keras.wrappers.scikit_learn.KerasClassifier(build_fn=models.get_l4_model, epochs=10, verbose=True)
     l4_model._estimator_type = "classifier"
 
-    ensemble = VotingClassifier(estimators=[('l1', l1_model),
+    ensemble12 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l2', l2_model)],
+                                voting='soft', verbose=False)
+    ensemble13 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l3', l3_model)],
+                                voting='soft', verbose=False)
+    ensemble14 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l4', l4_model)],
+                                voting='soft', verbose=False)
+    ensemble23 = VotingClassifier(estimators=[('l2', l2_model),
+                                            ('l3', l3_model)],
+                                voting='soft', verbose=False)
+    ensemble24 = VotingClassifier(estimators=[('l2', l2_model),
+                                            ('l4', l4_model)],
+                                voting='soft', verbose=False)
+    ensemble34 = VotingClassifier(estimators=[('l3', l3_model),
+                                            ('l4', l4_model)],
+                                voting='soft', verbose=False)
+
+    ensemble123 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l2', l2_model),
+                                            ('l3', l3_model)],
+                                voting='soft', verbose=False)
+    ensemble124 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l2', l2_model),
+                                            ('l4', l4_model)],
+                                voting='soft', verbose=False)
+    ensemble134 = VotingClassifier(estimators=[('l1', l1_model),
+                                            ('l3', l3_model),
+                                            ('l4', l4_model)],
+                                voting='soft', verbose=False)
+    ensemble234 = VotingClassifier(estimators=[('l2', l2_model),
+                                            ('l3', l3_model),
+                                            ('l4', l4_model)],
+                                voting='soft', verbose=False)
+
+
+    ensemble1234 = VotingClassifier(estimators=[('l1', l1_model),
                                             ('l2', l2_model),
                                             ('l3', l3_model),
                                             ('l4', l4_model)],
-                                voting='soft')
+                                voting='soft', verbose=False)
+
+    l1_model.fit(x_train, y_train)
+    l2_model.fit(x_train, y_train)
+    l3_model.fit(x_train, y_train)
+    l4_model.fit(x_train, y_train)
+
+    ensemble12.fit(x_train, y_train)
+    ensemble13.fit(x_train, y_train)
+    ensemble14.fit(x_train, y_train)
+    ensemble23.fit(x_train, y_train)
+    ensemble24.fit(x_train, y_train)
+    ensemble34.fit(x_train, y_train)
+
+    ensemble123.fit(x_train, y_train)
+    ensemble124.fit(x_train, y_train)
+    ensemble134.fit(x_train, y_train)
+    ensemble234.fit(x_train, y_train)
+
+    ensemble1234.fit(x_train, y_train)
+
+    for clf in (l1_model, l2_model, l3_model, l4_model,
+                ensemble12, ensemble13, ensemble14, ensemble23, ensemble24, ensemble34,
+                ensemble123, ensemble124, ensemble134, ensemble234,
+                ensemble1234):
+        before_time = time.time()
+        y_pred = clf.predict(x_test)
+        model_time = time.time() - before_time
+
+        print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
+        print("Time: ", model_time)
 
     l1_model.fit(x_train, y_train)
     l2_model.fit(x_train, y_train)
