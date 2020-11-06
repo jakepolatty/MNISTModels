74ecf79d21ec70dd69e3406b5c9adf01ec218423
Jake Polatty <jakepolatty@gmail.com>
2020-11-05 19:51:18 -0500

    Final MNIST stacking experiment

--- DiffNumModelCombinations/mnist_stacking_experiment.py
+++ DiffNumModelCombinations/mnist_stacking_experiment.py
@@ -31,73 +31,87 @@ def main():
 
     ensemble12 = StackingClassifier(estimators=[('l1', l1_model),
                                             ('l2', l2_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble13 = StackingClassifier(estimators=[('l1', l1_model),
                                             ('l3', l3_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble14 = StackingClassifier(estimators=[('l1', l1_model),
                                             ('l4', l4_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble23 = StackingClassifier(estimators=[('l2', l2_model),
                                             ('l3', l3_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble24 = StackingClassifier(estimators=[('l2', l2_model),
                                             ('l4', l4_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble34 = StackingClassifier(estimators=[('l3', l3_model),
                                             ('l4', l4_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble123 = StackingClassifier(estimators=[('l1', l1_model),
                                             ('l2', l2_model),
                                             ('l3', l3_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble124 = StackingClassifier(estimators=[('l1', l1_model),
                                             ('l2', l2_model),
                                             ('l4', l4_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble134 = StackingClassifier(estimators=[('l1', l1_model),
                                             ('l3', l3_model),
                                             ('l4', l4_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble234 = StackingClassifier(estimators=[('l2', l2_model),
                                             ('l3', l3_model),
                                             ('l4', l4_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     ensemble1234 = StackingClassifier(estimators=[('l1', l1_model),
                                             ('l2', l2_model),
                                             ('l3', l3_model),
                                             ('l4', l4_model)],
-                                final_estimator=LogisticRegression())
+                                final_estimator=HistGradientBoostingClassifier(random_state=42))
 
     l1_model.fit(x_train, y_train2)
+    #print("L1 Done")
     l2_model.fit(x_train, y_train2)
+    #print("L2 Done")
     l3_model.fit(x_train, y_train2)
+    #print("L3 Done")
     l4_model.fit(x_train, y_train2)
-    ensemble.fit(x_train, y_train)
+    #print("L4 Done")
 
     ensemble12.fit(x_train, y_train)
+    #print("L12 Done")
     ensemble13.fit(x_train, y_train)
+    #print("L13 Done")
     ensemble14.fit(x_train, y_train)
+    #print("L14 Done")
     ensemble23.fit(x_train, y_train)
+    #print("L23 Done")
     ensemble24.fit(x_train, y_train)
+    #print("L24 Done")
     ensemble34.fit(x_train, y_train)
+    #print("L34 Done")
 
     ensemble123.fit(x_train, y_train)
+    #print("L123 Done")
     ensemble124.fit(x_train, y_train)
+    #print("L124 Done")
     ensemble134.fit(x_train, y_train)
+    #print("L134 Done")
     ensemble234.fit(x_train, y_train)
+    #print("L234 Done")
 
     ensemble1234.fit(x_train, y_train)
+    #print("L1234 Done")
 
     for clf in (l1_model, l2_model, l3_model, l4_model,
                 ensemble12, ensemble13, ensemble14, ensemble23, ensemble24, ensemble34,
