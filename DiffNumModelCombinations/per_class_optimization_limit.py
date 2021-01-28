ad126271e19bbc95d25430446c04a95abffa1043
Jake Polatty <jakepolatty@gmail.com>
2021-01-28 02:03:27 -0500

    Finished maximally optimized model generation

--- DiffNumModelCombinations/per_class_optimization_limit.py
+++ DiffNumModelCombinations/per_class_optimization_limit.py
@@ -10,8 +10,6 @@ def main():
     x_train, y_train, x_test, y_test = helpers.get_mnist_data()
     y_test = tf.squeeze(y_test)
 
-    #train_and_save_models(x_train, y_train)
-
     print("Loading models...")
     l1_model = tf.keras.models.load_model('models/cifar/l1_model')
     l2_model = tf.keras.models.load_model('models/cifar/l2_model')
@@ -25,19 +23,38 @@ def main():
     l10_model = tf.keras.models.load_model('models/cifar/l10_model')
 
     models = [l1_model, l2_model, l3_model, l4_model, l5_model, l6_model, l7_model, l8_model, l9_model, l10_model]
-    accuracies = compute_class_matrix(models, y_test)
+    class_count = 10
+    accuracies = compute_class_matrix(models, class_count, y_test)
 
-    
+    best_models = np.argmax(accuracies, axis=0)
+
+    # Classify the test data into buckets based off of their true class
+    buckets = [[] for i in range(class_count)]
+    for i in range(x_test.shape[0]):
+        class_peek = y_test[i]
+        buckets[class_peek].append(x_test[i])
+
+    # Loop through each of the class buckets and run the corresponding model, before adding up the correct predictions
+    total_correct = 0 
+    for i in range(class_count):
+        bucket_inputs = np.array(buckets[i])
+        probs = model.predict(bucket_inputs)
+        preds = np.argmax(probs, axis=1)
+        total_correct += np.sum(preds == i)
+
+    print("Final accuracy: ", total_correct / x_test.shape[0])
 
 
-def compute_class_matrix(models, y_test):
+
+
+def compute_class_matrix(models, class_count, y_test):
     # Get dictionary of counts of each class in y_test
     y_test_np = y_test.numpy()
     count_dicts = []
 
     # Set up accuracy grid
     model_count = len(models)
-    accuracies = np.zeros((model_count, model_count))
+    accuracies = np.zeros((model_count, class_count))
 
     # Iterate over all models and get their predicted outputs
     for i in range(model_count):
@@ -50,7 +67,7 @@ def compute_class_matrix(models, y_test):
         count_dicts.append(dict(zip(unique, counts)))
 
         # Iterate over all 10 classes
-        for j in range(model_count):
+        for j in range(class_count):
             # Compute the number of times where the prediction matches the test output for that class
             class_count = len(np.where((model_preds == j) & (y_test_np == j))[0])
             accuracies[i][j] = class_count / count_dicts[i][j]
