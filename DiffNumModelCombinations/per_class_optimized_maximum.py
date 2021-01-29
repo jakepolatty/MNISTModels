5bdd45e88dfd767d53931213d9567d796e3a8e78
Jake Polatty <jakepolatty@gmail.com>
2021-01-29 00:17:22 -0500

    Structure for per-class weighted maximum implementation

---
+++ DiffNumModelCombinations/per_class_optimized_maximum.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+import time
+import numpy as np
+
+import helpers.helper_funcs as helpers
+#import helpers.cifar_models as models
+
+#RUN ALL 10 MODELS, PICK HEIGHEST PROBABILITY IN ENTIRE GRID
+#RUN ALL 10 MODELS, PICK HEIGHEST PROBABILITY WEIGHTED BY A OR B
+
+def main():
+    print('Loading data...')
+    x_train, y_train, x_test, y_test = helpers.get_cifar10_data()
+    y_test = tf.squeeze(y_test)
+
+    print("Loading models...")
+    l1_model = tf.keras.models.load_model('models/cifar/l1_model')
+    l2_model = tf.keras.models.load_model('models/cifar/l2_model')
+    l3_model = tf.keras.models.load_model('models/cifar/l3_model')
+    l4_model = tf.keras.models.load_model('models/cifar/l4_model')
+    l5_model = tf.keras.models.load_model('models/cifar/l5_model')
+    l6_model = tf.keras.models.load_model('models/cifar/l6_model')
+    l7_model = tf.keras.models.load_model('models/cifar/l7_model')
+    l8_model = tf.keras.models.load_model('models/cifar/l8_model')
+    l9_model = tf.keras.models.load_model('models/cifar/l9_model')
+    l10_model = tf.keras.models.load_model('models/cifar/l10_model')
+
+    models = [l1_model, l2_model, l3_model, l4_model, l5_model, l6_model, l7_model, l8_model, l9_model, l10_model]
+    num_classes = 10
+    accuracies = compute_class_matrix_A(models, num_classes, x_test, y_test)
+
+    best_models = np.argmax(accuracies, axis=0)
+
+    # Loop through each of the class buckets and run the corresponding model, before adding up the correct predictions
+    total_correct = 0 
+
+    print("Final accuracy: ", total_correct / x_test.shape[0])
+
+
+def compute_class_matrix_A(models, num_classes, x_test, y_test):
+    # Get dictionary of counts of each class in y_test
+    y_test_np = y_test.numpy()
+    unique, counts = np.unique(y_test_np, return_counts=True)
+    count_dict = dict(zip(unique, counts))
+
+    # Set up accuracy grid
+    num_models = len(models)
+    accuracies = np.zeros((num_models, num_classes))
+
+    # Iterate over all models and get their predicted outputs
+    for i in range(num_models):
+        model = models[i]
+
+        model_probs = model.predict(x_test)
+        model_preds = np.argmax(model_probs, axis=1)
+
+        # Iterate over all 10 classes
+        for j in range(num_classes):
+            # Compute the number of times where the prediction matches the test output for that class
+            class_count = len(np.where((model_preds == j) & (y_test_np == j))[0])
+            accuracies[i][j] = class_count / count_dict[j]
+
+    print(accuracies)
+    return accuracies
+
+
+def compute_class_matrix_B(models, num_classes, x_test, y_test):
+    # Get dictionary of counts of each class in y_test
+    y_test_np = y_test.numpy()
+    count_dicts = []
+
+    # Set up accuracy grid
+    num_models = len(models)
+    accuracies = np.zeros((num_models, num_classes))
+
+    # Iterate over all models and get their predicted outputs
+    for i in range(num_models):
+        model = models[i]
+
+        model_probs = model.predict(x_test)
+        model_preds = np.argmax(model_probs, axis=1)
+
+        unique, counts = np.unique(model_preds, return_counts=True)
+        count_dicts.append(dict(zip(unique, counts)))
+
+        # Iterate over all 10 classes
+        for j in range(num_classes):
+            # Compute the number of times where the prediction matches the test output for that class
+            class_count = len(np.where((model_preds == j) & (y_test_np == j))[0])
+            accuracies[i][j] = class_count / count_dicts[i][j]
+
+    print(accuracies)
+    return accuracies
+
+
+if __name__ == '__main__':
+    main()
